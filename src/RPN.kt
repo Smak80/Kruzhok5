c88835127f801fa5a4cf9f05475ca6dcaed0bbc2
Сергей Владиславович Маклецов <smak.80@yandex.ru>
2025-05-12 17:25:10 +0300

    Разделение рабочая версия

--- src/RPN.kt
+++ src/RPN.kt
@@ -1,26 +1,30 @@
-enum class CharType{LETTER,DIGIT,OTHER,SPACE}
+
+fun examinationPush (prev: Char?, ch:Char) = (prev==null)
+    || (prev.isLetter() && ch.isLetter())
+    || (prev.isDigit() && ch.isDigit())
+    || (prev.isLetter() && ch.isDigit())
+
+
 fun String.splitMath(): List<String>{
     val result = mutableListOf<String>()
-    var prevCharType: CharType? = null
-    fold(""){acc, ch ->
-        val charType = when{
-            ch.isLetter() -> CharType.LETTER
-            ch.isDigit() -> CharType.DIGIT
-            ch.isWhitespace() -> CharType.SPACE
-            else -> CharType.OTHER
-        }
-        if (/*добавить символ в строку*/ ){
-            acc + ch
-        }else{
-            result.add(acc)
-            "" + ch
-        }
-
+    var prev:Char?=null
+    return result.run{
+        add(this@splitMath.fold(""){ acc, ch ->
+            if (prev == null || !examinationPush(prev,ch)){
+                prev=ch
+                result.add(acc)
+                "" + ch
+            }else{
+                acc + ch
+            }
+        })
+        filter{it.isNotBlank()}
     }
-    return result
 }
 fun main(){
-    println("2x^2+sin(x) * x2".splitMath())
+    println("2x^2+sin(x) * x2y".splitMath())
     // ["2","x","^","2","+","sin","(","x",")","*","x2"]
 //    println(listOf(3, 2, 5, 4, 7).fold(0){ acc, value -> if (value % 2 == 0) acc - value else acc + value })
+    println("2+2*2".splitMath())
+
 }
